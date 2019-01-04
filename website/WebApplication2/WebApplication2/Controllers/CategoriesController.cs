9eefc1c38f18ff37d8c30fdf85f31c452085d141
NaeemaHasan <sh01703@st.habib.edu.pk>
2019-01-04 15:44:03 +0500

    asp login done

---
+++ website/WebApplication2/WebApplication2/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    [Authorize]
+    public class CategoriesController : ApiController
+    {
+        public IEnumerable<Category> Get()
+        {
+            using (fypEntities entities = new fypEntities())
+            {
+                return entities.Categories.ToList();
+            }
+        }
+    }
+}
