b7e8825acd9bab2d36485adf96e2d6e0e9b7a713
NaeemaHasan <sh01703@st.habib.edu.pk>
2019-01-25 16:17:35 +0500

    sprint 3 website

---
+++ website/sprint3/WebApplication2/WebApplication2/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Class1
+    {
+        public String code;
+        public Double price;
+        public String name;
+    }
+}
