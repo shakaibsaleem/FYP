5cf01b556e871320f798d52ae00793a7d2673b4a
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-02-04 14:53:58 +0500

    connecting and reading to database

--- scraperToDatabase/learn.py
+++ scraperToDatabase/learn.py
@@ -1,6 +1,48 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table
 import pandas as pd
+import urllib
 
 # Read file into a DataFrame and print its head
-df = pd.read_csv('khaadi.csv')
-print(df.head())
+# df = pd.read_csv('khaadi.csv')
+# print(df.head())
+
+# conString = "Data Source = .\\SQLEXPRESS;Initial Catalog = fyp; Persist Security Info=True;User ID = sa; Password=studio19;"
+
+# conString = "mssql+pyodbc://sa:studio19@SQLEXPRESS/fyp"
+
+params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};SERVER=.\\SQLEXPRESS;DATABASE=fyp;UID=sa;PWD=studio19")
+conString = "mssql+pyodbc:///?odbc_connect=%s" % params
+
+engine = create_engine(conString)
+
+mytables = engine.table_names()
+
+# print(tables)
+
+metadata = MetaData()
+
+# p = tables[4]
+# print(p)
+
+product_table = Table(mytables[4], metadata, autoload = True, autoload_with = engine)
+
+# print(repr(product_table))
+# print(repr(metadata.tables[mytables[4]]))
+
+# print(product_table.columns.keys())
+
+connection = engine.connect()
+
+select_stmt = "select * from "
+
+# ResultProxy
+result_proxy = connection.execute(select_stmt + mytables[4])
+
+# ResultSet
+results = result_proxy.fetchall()
+
+# print(product_table.columns.keys())
+# print(results)
+
+# first_row = results[0]
+# print(first_row)
