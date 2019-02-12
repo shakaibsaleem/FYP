32f521f0436e6a152010788af9f64acd14b93e0d
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-02-12 12:06:53 +0500

    Update learn.py

--- scraperToDatabase/learn.py
+++ scraperToDatabase/learn.py
@@ -70,3 +70,46 @@ results_where = connection.execute(select_where).fetchall()
 df2 = pd.DataFrame(results_where)
 df2.columns = results_where[0].keys()
 print(df2)
+
+
+'''
+# Import insert and select from sqlalchemy
+from sqlalchemy import insert, select
+
+# Build an insert statement to insert a record into the data table: stmt
+stmt = insert(data).values(name="Anna", count=1, amount=1000.00, valid=True)
+
+# Execute the statement via the connection: results
+results = connection.execute(stmt)
+
+# Print result rowcount
+print(results.rowcount)
+
+# Build a select statement to validate the insert
+stmt = select([data]).where(data.columns.name == "Anna")
+
+# Print the result of executing the query.
+print(connection.execute(stmt).first())
+
+
+
+
+
+
+
+# Build a list of dictionaries: values_list
+values_list = [
+    {'name': "Anna", 'count': 1, 'amount': 1000.00, 'valid': True},
+    {'name': "Taylor", 'count': 1, 'amount': 750.00, 'valid': False}
+]
+
+# Build an insert statement for the data table: stmt
+stmt = insert(data)
+
+# Execute stmt with the values_list: results
+results = connection.execute(stmt, values_list)
+
+# Print rowcount
+print(results.rowcount)
+'''
+
