494a42342363e9296dbdc562d64b9708ad5b44e6
alizarhassanali <alizarhassanali97@gmail.com>
2019-04-25 14:18:48 +0500

    Sprint 4
    organized every dip to sprint 4 folder

---
+++ sprint4/Classification/ART/python.py
@@ -0,0 +1,245 @@
+
+from __future__ import print_function
+from __future__ import division
+import numpy as np
+import csv
+import os
+import shutil
+from pathlib import Path
+import glob
+
+class ART:
+    ''' ART class
+    Usage example:
+    --------------
+    # Create a ART network with input of size 5 and 20 internal units
+    >>> network = ART(5,10,0.5)
+    '''
+
+    def __init__(self, n=5, m=10, rho=.5):
+        '''
+        Create network with specified shape
+        Parameters:
+        -----------
+        n : int
+            Size of input
+        m : int
+            Maximum number of internal units 
+        rho : float
+            Vigilance parameter
+        '''
+        # Comparison layer
+        self.F1 = np.ones(n)
+        # Recognition layer
+        self.F2 = np.ones(m)
+        # Feed-forward weights
+        self.Wf = np.random.random((m,n))
+        # Feed-back weights
+        self.Wb = np.random.random((n,m))
+        # Vigilance
+        self.rho = rho
+        # Number of active units in F2
+        self.active = 0
+
+
+    def learn(self, X):
+        ''' Learn X '''
+        
+        # Compute F2 output and sort them (I)
+        self.F2[...] = np.dot(self.Wf, X)
+       # print("This is WF",self.Wf)
+        I = np.argsort(self.F2[:self.active].ravel())[::-1]
+       # print("This is X",X)
+       # print("This is F2,I,self.active",self.F2,I,self.active)
+        for i in I:
+           # print(I,"This is I")
+            # Check if nearest memory is above the vigilance level
+           # d = sum(self.Wb[:,i]*X)/sum(X)
+            d = (self.Wb[:,i]*X).sum()/X.sum()
+          #  print("This is D",d)
+            if d >= self.rho:
+                # Learn data
+                self.Wb[:,i] *= X
+                self.Wf[i,:] = self.Wb[:,i]/(0.5+self.Wb[:,i].sum())
+                return self.Wb[:,i], i
+
+        # No match found, increase the number of active units
+        # and make the newly active unit to learn data
+        if self.active < self.F2.size:
+            i = self.active
+            self.Wb[:,i] *= X
+            self.Wf[i,:] = self.Wb[:,i]/(0.5+self.Wb[:,i].sum())
+            self.active += 1
+            return self.Wb[:,i], i
+
+        return None,None
+
+
+# -----------------------------------------------------------------------------
+if __name__ == '__main__':
+
+    np.random.seed(1)
+
+    # Example 1 : very simple data
+    # -------------------------------------------------------------------------
+    network = ART( 13, 1000, rho=0.52)
+    
+    with open('haralick_no_human25_4 colornormalized.csv') as f:
+        reader = csv.reader(f)
+        i = 0
+        data = []
+        name = []
+        for row in reader:
+            if(i==0):
+                i+=1
+                continue
+            name.append(row[0])
+            data.append ([ float(x) for x in row[1:] ])
+            i+=1
+
+##    data = ["   O ",
+##            "  O O",
+##            "    O",
+##            "  O O",
+##            "    O",
+##            "  O O",
+##            "    O",
+##            " OO O",
+##            " OO  ",
+##            " OO O",
+##            " OO  ",
+##            "OOO  ",
+##            "OO   ",
+##            "O    ",
+##            "OO   ",
+##            "OOO  ",
+##            "OOOO ",
+##            "OOOOO",
+##            "O    ",
+##            " O   ",
+##            "  O  ",
+##            "   O ",
+##            "    O",
+##            "  O O",
+##            " OO O",
+##            " OO  ",
+##            "OOO  ",
+##            "OO   ",
+##            "OOOO ",
+##            "OOOOO"]
+    results = {}
+    X = np.zeros(len(data[0]))
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            X[j] = (data[i][j] >=0.5)
+            
+        Z, k = network.learn(X)
+        # if k == 
+##        print(name[i],k)
+        results[name[i]] = k
+        # print("|%s|"%name[i],"-> class", k)
+        
+    searchPath = 'C:\\Users\\al00993\\Documents\\GitHub\\FYP\\sprint4\\data\\images\\sachwani_cropped_images\\Yelo cropped\\**\\*.jpg'
+    newpa = "C:\\Users\\al00993\\Documents\\GitHub\\FYP\\sprint4\\Classification\\ART\data\\"
+    num = 0
+    splitt = '\\'
+##    print("Hello",splitt)
+##    print('result List', resultList)
+##    print('type',typ)
+##    print('n',n)
+    for i in range(10):
+        newpath = newpa  +'ClustersnewN' + 'Label'+str(i)
+        try:
+            os.mkdir(newpath)
+        except OSError:
+            print ("Creation of the directory %s failed" % newpath)
+
+            
+ #        else:
+ #            print ("Successfully created the directory %s " % newpath)
+#         newpath = newpa + typ +'ClustersnewN' +str(n) + 'Label'+str(num)
+#         
+    x=glob.glob(searchPath, recursive=True)
+##    print(x)
+    for filename in x:
+##        print(filename)
+        for j in results:
+##            print(j)
+            if j == filename.split(splitt)[-1]:
+##                print('yaha',filename)
+                shutil.copy2(filename , newpa + 'ClustersnewN' + 'Label'+ str(results[j]))
+                continue
+   
+    
+    # Example 2 : Learning letters
+    # -------------------------------------------------------------------------
+##    def letter_to_array(letter):
+##        ''' Convert a letter to a numpy array '''
+##        shape = len(letter), len(letter[0])
+##        Z = np.zeros(shape, dtype=int)
+##        for row in range(Z.shape[0]):
+##            for column in range(Z.shape[1]):
+##                if letter[row][column] == '#':
+##                    Z[row][column] = 1
+##        return Z
+##
+##    def print_letter(Z):
+##        ''' Print an array as if it was a letter'''
+##        for row in range(Z.shape[0]):
+##            for col in range(Z.shape[1]):
+##                if Z[row,col]:
+##                    print( '#', end="" )
+##                else:
+##                    print( ' ', end="" )
+##            print( )
+##       
+##    A = letter_to_array( [' #### ',
+##                          '#    #',
+##                          '#    #',
+##                          '######',
+##                          '#    #',
+##                          '#    #',
+##                          '#    #'] )
+##    B = letter_to_array( ['##### ',
+##                          '#    #',
+##                          '#    #',
+##                          '##### ',
+##                          '#    #',
+##                          '#    #',
+##                          '##### '] )
+##    C = letter_to_array( [' #### ',
+##                          '#    #',
+##                          '#     ',
+##                          '#     ',
+##                          '#     ',
+##                          '#    #',
+##                          ' #### '] )
+##    D = letter_to_array( ['##### ',
+##                          '#    #',
+##                          '#    #',
+##                          '#    #',
+##                          '#    #',
+##                          '#    #',
+##                          '##### '] )
+##    E = letter_to_array( ['######',
+##                          '#     ',
+##                          '#     ',
+##                          '####  ',
+##                          '#     ',
+##                          '#     ',
+##                          '######'] )
+##    F = letter_to_array( ['######',
+##                          '#     ',
+##                          '#     ',
+##                          '####  ',
+##                          '#     ',
+##                          '#     ',
+##                          '#     '] )
+##
+##    samples = [A,B,C,D,E,F]
+##    network = ART( 6*7, 10, rho=0.15 )
+##
+##    for i in range(len(samples)):
+##        Z, k = network.learn(samples[i].ravel())
+##        print("%c"%(ord('A')+i),"-> class",k)
+##        print_letter(Z.reshape(7,6))
---
+++ sprint4/Classification/SOM/SOM.py
@@ -0,0 +1,230 @@
+from __future__ import division
+
+import numpy as np
+##from matplotlib import pyplot as plt
+##from matplotlib import patches as patches
+import csv
+import os
+from pathlib import Path
+import shutil
+import glob
+
+
+def SOM(w,l):
+    PointDictionary = dict()
+    def ge1tPoints(file):
+        # gets points from csv file
+        rd = csv.reader(open(file))
+        names = []
+        feats = []
+        temp = []
+        vectorlist = []
+        for row in rd:
+            names.append(row[0])
+            feats.append(row[1:])
+        for l in feats[1:]:
+            temp.append([float(i) for i in l])
+
+        names = names[1:]
+        newDict = dict()
+        for i in range(len(temp)):
+            newDict[tuple(temp[i])] = names[i]
+        # return temp,newDict
+        for i in newDict:
+            vectorlist.append(list(i))
+        
+        vectorlistnp = np.array(vectorlist)
+        vectorarray = vectorlistnp.T
+        for i in range(len(vectorlistnp)):
+            templist = []
+            for j in vectorlistnp[i]:
+                    templist.append((j))
+            PointDictionary[str(templist)] = names[i]
+        # print(vectorarray)
+        return vectorarray
+
+
+
+
+
+    raw_data = ge1tPoints('haralick_no_human25_4 colornormalized.csv')
+
+
+    network_dimensions = np.array([w, l])
+    n_iterations = 100000
+    init_learning_rate = 0.01
+
+    normalise_data = False
+    normalise_by_column = False
+
+    # print(raw_data)
+    m = raw_data.shape[0]
+    # print(m)
+    n = raw_data.shape[1]
+    # print(n)
+
+    # initial neighbourhood radius
+    init_radius = max(network_dimensions[0], network_dimensions[1]) / 2
+    # radius decay parameter
+    time_constant = n_iterations / np.log(init_radius)
+
+    data = raw_data
+
+    if normalise_data:
+        if normalise_by_column:
+            col_maxes = raw_data.max(axis=0)
+            print(col_maxes)
+            data = raw_data / col_maxes[np.newaxis, :]
+            # print('data',raw_data)
+            # print('nordata',data)
+        else:
+            data = raw_data / data.max()
+
+    net = np.random.random((network_dimensions[0], network_dimensions[1], m))
+
+
+    def find_bmu(t, net, m):
+        """
+            Find the best matching unit for a given vector, t
+            Returns: bmu and bmu_idx is the index of this vector in the SOM
+        """
+        bmu_idx = np.array([0, 0])
+        min_dist = np.iinfo(np.int).max
+        
+        # calculate the distance between each neuron and the input
+        for x in range(net.shape[0]):
+            for y in range(net.shape[1]):
+                w = net[x, y, :].reshape(m, 1)
+                sq_dist = np.sum((w - t) ** 2)
+                sq_dist = np.sqrt(sq_dist)
+                if sq_dist < min_dist:
+                    min_dist = sq_dist # dist
+                    bmu_idx = np.array([x, y]) # id
+        
+        bmu = net[bmu_idx[0], bmu_idx[1], :].reshape(m, 1)
+        # print('bmu', (bmu, bmu_idx) )
+        return (bmu, bmu_idx)
+
+
+    def decay_radius(initial_radius, i, time_constant):
+        return initial_radius * np.exp(-i / time_constant)
+
+    def decay_learning_rate(initial_learning_rate, i, n_iterations):
+        return initial_learning_rate * np.exp(-i / n_iterations)
+
+    def calculate_influence(distance, radius):
+        return np.exp(-distance / (2* (radius**2)))
+
+    diction = {}
+
+    count = 0
+    for i in range(n_iterations):
+        # select a training example at random    
+        count += 1
+        t = data[:, np.random.randint(0, n)].reshape(np.array([m, 1]))
+        # print('t',t)
+        # for i in t:
+        # 	print(i)
+        # 	print(type(i))
+
+        # # print('yaha')
+        # print(t)
+        # print(type(t))
+                
+        # find its Best Matching Unit
+        bmu, bmu_idx = find_bmu(t, net, m)
+        
+
+        tt= []
+        for i in t:
+            for j in i:
+                    tt.append(j)
+        diction[str(tt)] = str(bmu_idx)
+        # print(t,'index',bmu_idx)
+        # decay the SOM parameters
+        r = decay_radius(init_radius, i, time_constant)
+        l = decay_learning_rate(init_learning_rate, i, n_iterations)
+        
+        # update weight vector to move closer to input
+        # and move its neighbours in 2-D vector space closer
+        
+        for x in range(net.shape[0]):
+            for y in range(net.shape[1]):
+                w = net[x, y, :].reshape(m, 1)
+                w_dist = np.sum((np.array([x, y]) - bmu_idx) ** 2)
+                w_dist = np.sqrt(w_dist)
+                
+                if w_dist <= r:
+                    # calculate the degree of influence (based on the 2-D distance)
+                    influence = calculate_influence(w_dist, r)
+                    # print(influence)
+                    # new w = old w + (learning rate * influence * delta)
+                    # where delta = input vector (t) - old w
+                    new_w = w + (l * influence * (t - w))
+                    net[x, y, :] = new_w.reshape(1, 13)
+    # NameClus = {}
+    # print(diction)
+    # print((PointDictionary))
+    # for key, value in PointDictionary.items():
+    # 	NameClus[key] = str(diction[value])
+
+    classes = []
+    for x,y in diction.items():
+            classes.append(y)
+
+    classes = set(classes)
+    c=[]
+    for i in classes:
+            temp = [PointDictionary[x] for x,y in diction.items() if y == i]
+            c.append(temp)
+    # for k , v in (diction.items()):
+    # 	# print('k',k)
+    # 	# print('v',v)
+    # 	temp = []
+
+    # 	for x,y in PointDictionary.items():
+    # 		if y == k:
+    # 			temp.append(x)
+
+    # 	c[v] = temp
+
+            # c[v] = [x for x,y in (PointDictionary.items()) if y==k ]
+
+    # print(c)
+
+    def folderDist(resultList,typ,n):
+        searchPath = 'C:\\Users\\al00993\\Documents\\GitHub\\FYP\\sprint4\\data\\images\\sachwani_cropped_images\\Yelo cropped\\**\\*.jpg'
+        newpa = "C:\\Users\\al00993\\Documents\\GitHub\\FYP\\sprint4\\Classification\\SOM\data\\"
+        num = 0
+        splitt = '\\'
+    ##    print("Hello",splitt)
+    ##    print('result List', resultList)
+    ##    print('type',typ)
+    ##    print('n',n)
+        for i in resultList:
+            num += 1
+            newpath = newpa  +'ClustersnewN' + str(typ) +str(n) + 'Label'+str(num)
+            try:
+                os.mkdir(newpath)
+            except OSError:
+                print ("Creation of the directory %s failed" % newpath)
+    #        else:
+    #            print ("Successfully created the directory %s " % newpath)
+            x=glob.glob(searchPath, recursive=True)
+            for j in i:
+    ##            imgFile = Path(searchPath + '/' + j)
+                for filename in x:
+                    #print(j,filename.split('\\')[-1])
+                    #print(filename.split('\\')[-1])
+                    if j == filename.split(splitt)[-1]:
+                        print('yaha')#,filename)
+                        shutil.copy2(filename , newpath + '/' + j)
+                        continue
+
+
+
+    folderDist(c,network_dimensions[0],network_dimensions[1])
+
+for i in range(6):
+    for j in range(6):
+        SOM(i+2,j+2)
